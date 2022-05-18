92854f34ae1685d520613209ab4aa8004d857c7b
ankit9835 <ankydchrismatic@gmail.com>
2022-05-18 21:47:26 +0530

    menu project going on

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,53 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const[loading,setLoading] = useState(true);
+  const[tours,setTour] = useState([]);
+
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id)
+    setTour(newTour);
+  }
+
+  const fetchTour = async () => {
+    setLoading(true);
+      try{
+        const response = await fetch(url);
+        const tours = await response.json();
+        setLoading(false);
+        setTour(tours);
+        console.log(tours);
+      } catch(error) {
+        setLoading(false);
+      }
+  }
+
+  useEffect(() => {
+    fetchTour()
+  }, []);
+
+  if(loading){
+    return (
+      <main>
+          <Loading />
+      </main>
+    )
+  }
+  if(tours.length === 0){
+    return (
+      <main>
+          Tours Not Available
+          <button className='btn' onClick={fetchTour}>
+            Refresh
+          </button>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  )
 }
 
 export default App
