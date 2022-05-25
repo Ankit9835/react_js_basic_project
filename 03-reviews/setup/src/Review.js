7944219be894ad39a18c82c5f6bc42f969f9045c
ankit9835 <ankydchrismatic@gmail.com>
2022-05-25 18:00:56 +0530

    upto lorem ipsum

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -6,6 +6,7 @@ const Review = () => {
   const [index,setIndex] = useState(0);
   const {id,name,job,image,text} = people[index];
 
+
   const countNumber = (number) => {
     if(number > people.length - 1){
         return 0;
